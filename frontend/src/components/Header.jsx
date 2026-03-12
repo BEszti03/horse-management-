d765e48bbd6d84e5c047937b2875c355fb376f3f
Eszter <bancsicse@freemail.hu>
2026-03-12 14:10:09 +0100

    adding logo to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import Menu from "./Menu";
 import "./Header.css";
+import logo from "../assets/logo.png";
 
 function Header() {
   const navigate = useNavigate();
@@ -12,7 +13,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo" onClick={handleLogoClick}>
-        LOGO
+        <img src={logo} alt="Horse-Time Management logo" />
       </div>
 
       <Menu />
